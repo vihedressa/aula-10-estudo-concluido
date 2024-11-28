76bc6307148b9bc60b35ec6aa7b2133008444779
vihedressa <vihanimes@gmail.com>
2024-11-28 08:06:59 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,63 @@
+criaCartao(
+    'Programação',
+    'O que é Python?',
+    'O Python é uma linguagem de programação'
+)
+
+criaCartao(
+    'Geografia',
+    'Qual a capital da França?',
+    'A capital da França é Paris'
+)
+
+criaCartao(
+    'Programação',
+    'O que é uma função?',
+    'Uma função é um bloco de código que executa alguma tarefa'
+)
+
+criaCartao(
+    'Lingua inglesa',
+    'Como se diz oi em Inglês?',
+    'Oi em ingles é HI (RAI)'
+)
+criaCartao(
+    'Matematica',
+    'Definição de derivada',
+    'Taxa de variação instantânea'
+)
+criaCartao(
+    'Matematica',
+    'Soma dos ângulos internos de um triângulo',
+    '180 graus'
+)
+criaCartao(
+    'Física',
+    'O que é força?',
+    'interação que altera o estado de movimento de um corpo'
+)
+criaCartao(
+    'Física',
+    'Qual unidade de medida da energia no SI',
+    'Joule (J)'
+)
+criaCartao(
+    'Física',
+    'O que diz a lei da Inércia?',
+    'um corpo tende a permanecer em repouso ou em movimento uniforme, salvo ação de uma força'
+)
+criaCartao(
+    'Programação',
+    'O que é uma variável?',
+    'Espaço na memória para armazenar um valor'
+)
+criaCartao(
+    'Programação',
+    'Qual a função do comando print',
+    'exibir informações na tela'
+)
+criaCartao(
+    'Programação',
+    'O que é um loop?',
+    'estrutura que repete blocos de código'
+)
